7185910c78fa78f90e590b5a407cc3f3c6fc1379
Miguel dos Santos <migueldossantos0096@gmail.com>
2023-06-05 21:29:39 -0300

    validando campos forms

--- atividadeCrud/pages/cursos/form.js
+++ atividadeCrud/pages/cursos/form.js
@@ -5,12 +5,13 @@ import React from 'react'
 import { Button, Form } from 'react-bootstrap'
 import { useForm } from 'react-hook-form'
 import { FiSave } from 'react-icons/fi'
-import {AiOutlineRollback} from 'react-icons/ai'
+import { AiOutlineRollback } from 'react-icons/ai'
+import cursoValidator from '@/validators/cursoValidator'
 
 const Forms = () => {
 
   const { push } = useRouter()
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, formState: { errors } } = useForm();
 
   function salvar(dados) {
     const cursos = JSON.parse(window.localStorage.getItem('cursos')) || []
@@ -25,17 +26,29 @@ const Forms = () => {
       <Form>
         <Form.Group className="mb-3" controlId="nome">
           <Form.Label>Nome</Form.Label>
-          <Form.Control type="text" placeholder="Digite o nome do curso" {...register('nome')} />
+          <Form.Control type="text"  placeholder="Digite o nome do curso" {...register('nome', cursoValidator.nome)} />
+          {
+            errors.nome &&
+            <p className='text-danger mt-1'>{errors.nome.message}</p>
+          }
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="duracao">
           <Form.Label>Duração</Form.Label>
-          <Form.Control type="text" placeholder="Digite a duração" {...register('duracao')} />
+          <Form.Control type="text" placeholder="Digite a duração" {...register('duracao', cursoValidator.duracao)} />
+          {
+            errors.duracao &&
+            <p className='text-danger mt-1'>{errors.duracao.message}</p>
+          }
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="modalidade">
           <Form.Label>Modalidade</Form.Label>
-          <Form.Control type="text" placeholder="Digite a modalidade" {...register('modalidade')} />
+          <Form.Control type="number" placeholder="Digite a modalidade" {...register('modalidade', cursoValidator.modalidade)} />
+          {
+            errors.modalidade &&
+            <p className='text-danger mt-1'>{errors.modalidade.message}</p>
+          }
         </Form.Group>
 
         <div className='text-center'>
@@ -44,7 +57,7 @@ const Forms = () => {
             Salvar
           </Button>
           <Link className='ms-2 btn btn-danger' href="/cursos">
-            <AiOutlineRollback className='me-2'/>
+            <AiOutlineRollback className='me-2' />
             Voltar
           </Link>
         </div>
---
+++ atividadeCrud/validators/cursoValidator.js
@@ -0,0 +1,40 @@
+const cursoValidator = {
+    nome:  {
+        required: 'Campo Obrigatório',
+        minLength: {
+          value: 3,
+          message: 'O mínimo é 3',
+        },
+        maxLength: {
+          value: 10,
+          message: 'O máximo é 10'
+        },
+      },
+
+      duracao: {
+        required: 'Campo Obrigatório',
+        minLength: {
+          value: 3,
+          message: 'O mínimo é 3',
+        },
+        maxLength: {
+          value: 10,
+          message: 'O máximo é 10'
+        },
+      },
+
+      modalidade: {
+        required: 'Campo Obrigatório',
+        minLength: {
+          value: 3,
+          message: 'O mínimo é 3',
+        },
+        maxLength: {
+          value: 10,
+          message: 'O máximo é 10'
+        },
+      }
+    
+}
+
+export default cursoValidator
